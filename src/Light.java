e03233ea566afbed3f8679879a238069371c916b
Vương Linh <Linh.VuongBao@Student.HTW-Berlin.de>
2025-04-28 22:23:41 +0200

    modified the light class

--- src/Light.java
+++ src/Light.java
@@ -1,3 +1,11 @@
 public class Light {
+    public Vector3 position;
+    public double intensity;
+    public ColorRGB color;
 
+    public Light(Vector3 position, double intensity, ColorRGB color) {
+        this.position = position;
+        this.intensity = intensity;
+        this.color = color;
+    }
 }
--- src/Sphere.java
+++ src/Sphere.java
@@ -1,4 +1,29 @@
 public class Sphere {
+    public Vector3 center;
+    public double radius;
+    public Material material;
 
+    public Sphere(Vector3 center, double radius, Material material) {
+        this.center = center;
+        this.radius = radius;
+        this.material = material;
+    }
 
+    // Returns distance t if hit, otherwise -1
+    public double intersect(Ray ray) {
+        Vector3 L = ray.origin.subtract(center);
+        double a = 1.0;  // since ray.direction is normalized
+        double b = 2.0 * ray.direction.dot(L);
+        double c = L.dot(L) - radius * radius;
+
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) return -1;
+
+        double t1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        double t2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+
+        if (t1 > 0) return t1;
+        if (t2 > 0) return t2;
+        return -1;
+    }
 }
