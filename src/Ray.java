3e8b6049d87090127d7950fa681c3afc21b6e599
Vương Linh <Linh.VuongBao@Student.HTW-Berlin.de>
2025-04-28 22:21:40 +0200

    modified ray class

--- src/Ray.java
+++ src/Ray.java
@@ -1,4 +1,13 @@
 public class Ray {
+    public Vector3 origin;
+    public Vector3 direction;
 
+    public Ray(Vector3 origin, Vector3 direction) {
+        this.origin = origin;
+        this.direction = direction.normalize();
+    }
 
+    public Vector3 getPoint(double t) {
+        return origin.add(direction.multiply(t));
+    }
 }
