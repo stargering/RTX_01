f644254fc55794191c3687e6e9e73bc23630a25c
Vương Linh <Linh.VuongBao@Student.HTW-Berlin.de>
2025-04-28 22:24:50 +0200

    modified the colorrgb file and camera file

--- src/Camera.java
+++ src/Camera.java
@@ -1,3 +1,27 @@
 public class Camera {
+    public Vector3 position;
+    public Vector3 forward, right, up;
+    public double imagePlaneWidth, imagePlaneHeight;
 
+    public Camera(Vector3 position, Vector3 lookAt, double imgWidth, double imgHeight) {
+        this.position = position;
+        this.forward = lookAt.subtract(position).normalize();
+        this.right = new Vector3(0, 1, 0).cross(forward).normalize();
+        this.up = forward.cross(right).normalize();
+        this.imagePlaneWidth = imgWidth;
+        this.imagePlaneHeight = imgHeight;
+    }
+
+    public Ray generateRay(int x, int y, int resX, int resY) {
+        double u = (x + 0.5) / resX - 0.5;
+        double v = (y + 0.5) / resY - 0.5;
+
+        Vector3 pointOnPlane = position
+                .add(forward)
+                .add(right.multiply(u * imagePlaneWidth))
+                .add(up.multiply(v * imagePlaneHeight));
+
+        Vector3 dir = pointOnPlane.subtract(position);
+        return new Ray(position, dir);
+    }
 }
--- src/ColorRGB.java
+++ src/ColorRGB.java
@@ -1,3 +1,22 @@
 public class ColorRGB {
+    public double r, g, b;
 
+    public ColorRGB(double r, double g, double b) {
+        this.r = r; this.g = g; this.b = b;
+    }
+
+    public ColorRGB multiply(double scalar) {
+        return new ColorRGB(r * scalar, g * scalar, b * scalar);
+    }
+
+    public ColorRGB multiply(ColorRGB other) {
+        return new ColorRGB(r * other.r, g * other.g, b * other.b);
+    }
+
+    public int toRGB() {
+        int ir = (int)(Math.min(1.0, r) * 255);
+        int ig = (int)(Math.min(1.0, g) * 255);
+        int ib = (int)(Math.min(1.0, b) * 255);
+        return (ir << 16) | (ig << 8) | ib;
+    }
 }
